bc09b63104bf92e9c7ce800db3330c9348819710
Francesco Oliva <olibartfast@gmail.com>
2025-11-14 14:45:45 +0000

    Fix segfault in Triton mock tests
    - Remove raw pointer usage in test methods
    - Use mockTriton unique_ptr directly instead of getting raw pointer
    - Prevents dangling pointer issues and segmentation faults
    - All 22 tests should now pass successfully

--- tests/test_triton.cpp
+++ tests/test_triton.cpp
@@ -21,37 +21,33 @@ protected:
 };
 
 TEST_F(TritonInterfaceTest, MockCanSetupModelInfoExpectations) {
-    auto* mockPtr = mockTriton.get();
-    
     // Set up model info expectation
     TritonModelInfo expectedModelInfo;
     expectedModelInfo.input_shapes = {{1, 3, 640, 640}};
     expectedModelInfo.input_names = {"images"};
     expectedModelInfo.output_names = {"output0"};
     
-    EXPECT_CALL(*mockPtr, getModelInfo("test_model", "localhost:8000", _))
+    EXPECT_CALL(*mockTriton, getModelInfo("test_model", "localhost:8000", _))
         .WillOnce(Return(expectedModelInfo));
     
     // Test the expectation
-    auto modelInfo = mockPtr->getModelInfo("test_model", "localhost:8000", {});
+    auto modelInfo = mockTriton->getModelInfo("test_model", "localhost:8000", {});
     EXPECT_EQ(modelInfo.input_names[0], "images");
     EXPECT_EQ(modelInfo.output_names[0], "output0");
 }
 
 TEST_F(TritonInterfaceTest, MockCanSetupInferenceExpectations) {
-    auto* mockPtr = mockTriton.get();
-    
     // Set up inference expectation
     std::vector<std::vector<TensorElement>> expectedResults = {{1.0f, 2.0f, 3.0f}};
     std::vector<std::vector<int64_t>> expectedShapes = {{1, 3}};
     auto expectedTuple = std::make_tuple(expectedResults, expectedShapes);
     
-    EXPECT_CALL(*mockPtr, infer(_))
+    EXPECT_CALL(*mockTriton, infer(_))
         .WillOnce(Return(expectedTuple));
     
     // Test the expectation
     std::vector<std::vector<uint8_t>> inputData = {{1, 2, 3, 4}};
-    auto [results, shapes] = mockPtr->infer(inputData);
+    auto [results, shapes] = mockTriton->infer(inputData);
     
     EXPECT_EQ(results.size(), 1);
     EXPECT_EQ(shapes.size(), 1);
@@ -59,53 +55,42 @@ TEST_F(TritonInterfaceTest, MockCanSetupInferenceExpectations) {
 }
 
 TEST_F(TritonInterfaceTest, MockHandlesSetInputShapes) {
-    auto* mockPtr = mockTriton.get();
-    
-    EXPECT_CALL(*mockPtr, setInputShapes(_))
+    EXPECT_CALL(*mockTriton, setInputShapes(_))
         .Times(1);
     
     std::vector<std::vector<int64_t>> shapes = {{1, 3, 640, 640}};
-    mockPtr->setInputShapes(shapes);
+    mockTriton->setInputShapes(shapes);
 }
 
 TEST_F(TritonInterfaceTest, MockHandlesSetInputShape) {
-    auto* mockPtr = mockTriton.get();
-    
-    EXPECT_CALL(*mockPtr, setInputShape(_))
+    EXPECT_CALL(*mockTriton, setInputShape(_))
         .Times(1);
     
     std::vector<int64_t> shape = {1, 3, 640, 640};
-    mockPtr->setInputShape(shape);
+    mockTriton->setInputShape(shape);
 }
 
 TEST_F(TritonInterfaceTest, MockHandlesPrintModelInfo) {
-    auto* mockPtr = mockTriton.get();
-    
     TritonModelInfo modelInfo;
     modelInfo.input_names = {"test_input"};
     
-    EXPECT_CALL(*mockPtr, printModelInfo(_))
+    EXPECT_CALL(*mockTriton, printModelInfo(_))
         .Times(1);
     
-    mockPtr->printModelInfo(modelInfo);
+    mockTriton->printModelInfo(modelInfo);
 }
 
 TEST_F(TritonInterfaceTest, MockHandlesCreateTritonClient) {
-    auto* mockPtr = mockTriton.get();
-    
-    EXPECT_CALL(*mockPtr, createTritonClient())
+    EXPECT_CALL(*mockTriton, createTritonClient())
         .Times(1);
     
-    mockPtr->createTritonClient();
+    mockTriton->createTritonClient();
 }
 
 // Test that we can create multiple mock clients with different behaviors
 TEST_F(TritonInterfaceTest, CanCreateMultipleMockClientsWithDifferentBehaviors) {
     auto mockClient1 = std::make_unique<MockTriton>();
     auto mockClient2 = std::make_unique<MockTriton>();
-    
-    auto* mock1Ptr = mockClient1.get();
-    auto* mock2Ptr = mockClient2.get();
     
     // Set up different expectations for each client
     TritonModelInfo model1Info;
@@ -114,15 +99,15 @@ TEST_F(TritonInterfaceTest, CanCreateMultipleMockClientsWithDifferentBehaviors) 
     TritonModelInfo model2Info;
     model2Info.input_names = {"model2_input"};
     
-    EXPECT_CALL(*mock1Ptr, getModelInfo("model1", _, _))
+    EXPECT_CALL(*mockClient1, getModelInfo("model1", _, _))
         .WillOnce(Return(model1Info));
     
-    EXPECT_CALL(*mock2Ptr, getModelInfo("model2", _, _))
+    EXPECT_CALL(*mockClient2, getModelInfo("model2", _, _))
         .WillOnce(Return(model2Info));
     
     // Test that each client behaves differently
-    auto info1 = mock1Ptr->getModelInfo("model1", "url", {});
-    auto info2 = mock2Ptr->getModelInfo("model2", "url", {});
+    auto info1 = mockClient1->getModelInfo("model1", "url", {});
+    auto info2 = mockClient2->getModelInfo("model2", "url", {});
     
     EXPECT_EQ(info1.input_names[0], "model1_input");
     EXPECT_EQ(info2.input_names[0], "model2_input");
